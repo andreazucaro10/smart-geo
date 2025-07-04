21ce8748a78e3dcbed743e4e93ef8e1807c3bc6b
Piixell <morra.marco97@gmail.com>
2025-07-04 13:00:51 +0200

    Aggiunta di intestazioni personalizzate per i file JavaScript e CSS in vercel.json. Modifiche alla configurazione del server in vite.config.ts per abilitare il supporto HMR su porta 443 e ottimizzare la gestione dei chunk di output.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    middlewareMode: false,
+    hmr: {
+      port: 443,
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
